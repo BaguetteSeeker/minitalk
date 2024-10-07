9f45f0297a4663610e1d980ab391f91d11858d56
epinaud <enzo.pinaud+42@protonmail.com>
2024-10-07 12:35:59 +0200

    Implemented client feature which outputs each param2' char line by line in binary;

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 12:29:06 by epinaud           #+#    #+#             */
-/*   Updated: 2024/10/04 02:39:31 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/10/07 12:19:25 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,23 @@ int	main(int argc, char *argv[])
 {
 	if (argc != 3)
 		return (printerr("Insufficient params count during client call to main\n"));
+
+	unsigned char   bit;
+
+	while (*argv[2])
+	{
+		bit = 0b10000000;
+		while (bit > 0)
+		{
+			if (bit & (unsigned char)*argv[2])
+				ft_printf("1");
+			else
+				ft_printf("0");
+			bit >>= 1;
+		}
+		ft_putchar_fd('\n', 1);
+		argv[2]++;
+	}
 
 	kill(ft_atoi(argv[1]), ft_atoi(argv[2]));
 	return (0);
