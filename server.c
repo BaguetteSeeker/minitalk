2d75a4e85953fcb89867c605d8a4ea2b3d651e7b
epinaud <enzo.pinaud+42@protonmail.com>
2024-10-14 02:08:27 +0200

    Added str alloc baset on msglen; Str filling is segfaulting;

--- server.c
+++ server.c
@@ -6,23 +6,11 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 23:02:14 by epinaud           #+#    #+#             */
-/*   Updated: 2024/10/14 01:12:48 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/10/14 02:06:05 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define _POSIX_SOURCE 200809L
-#define _XOPEN_SOURCE 700
-#include <signal.h>
-#include "libft/libft.h"
-#include <stdio.h>
-
-typedef struct s_client
-{
-	int		client_pid;
-	char	*msg;
-	unsigned char	c;
-	int		counter;
-}			client;
+#include "minitalk.h"
 
 //client client_lst[];
 
@@ -59,24 +47,28 @@ void signals_handler(int sig, siginfo_t *siginfo, void *context)
 	static int	msglen = 0;
 	static int	bits_counter = 0;
 	static unsigned int	mxint_mask = 0b10000000000000000000000000000000;
+	static char	*msg;
 	(void)context;
 
 	//Check struct, if pid exist fetch progression else init struct
 	//if msglen == -1 if combytescount != 4 : msglen += parse_byte()
 	//else parse msg
 	bits_counter++;
-	if (mxint_mask == 0)
+	if (mxint_mask > 0)
 	{
 		if (sig == SIGUSR1)
 			msglen += mxint_mask;
 		mxint_mask >>= 1;
+		if (bits_counter == 32)
+		{
+			ft_printf("Bits counter is %d\n", bits_counter);
+			msg = malloc((msglen + 1) * sizeof(char));
+			bits_counter = 0;
+		}
 		//return ;
 	}
 	else
 	{
-		ft_printf("Strlen is %d\n", msglen);
-
-		//Malloc new chain and assign its address to the current struct
 		if (sig == SIGUSR1)
 			c += bit_mask;
 		bit_mask /= 2;
@@ -84,13 +76,14 @@ void signals_handler(int sig, siginfo_t *siginfo, void *context)
 		{
 			if (c == '\0')
 			{
+				ft_putstr_fd(msg - bits_counter / 8, 1);
 				ft_printf("'\n'Success; Client transmission over;\n");
-				//Full line transfered, print '\n'
+				free(msg - bits_counter / 8);
 				//Maybe end of messGE SUCH AS : Cliend <pid> successfully transmission over
 				//Free *str / reset struct
 			}
 			else
-				ft_putchar_fd(c, 1);
+				*msg++ = c;
 			//init_client_vars(&c, &counter, str);
 			//ft_putchar_fd('\n', 1);
 			c = 0;
@@ -100,21 +93,7 @@ void signals_handler(int sig, siginfo_t *siginfo, void *context)
 	if (siginfo->si_pid == 0)
         printerr("Server missing client's PID\n");
     if (kill(siginfo->si_pid, SIGUSR2) == -1)
-        printerr("Error in returning signal!\n");
-}
-
-static void set_sigaction(void (sighandle)(int, siginfo_t *, void *))
-{
-	struct sigaction act;
-
-	ft_bzero(&act, sizeof(act));
-	sigemptyset(&act.sa_mask);
-	act.sa_flags = SA_SIGINFO;
-	act.sa_sigaction = sighandle;
-	if (sigaction(SIGUSR1, &act, NULL) < 0)
-		return ;	
-	if (sigaction(SIGUSR2, &act, NULL) < 0)
-		return ;
+        printerr("Error in returning signal (to client)\n");
 }
 
 int	main(void)
