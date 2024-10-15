58854ee4b7fede2dc32a02cd57f2ecb26523d23a
epinaud <enzo.pinaud+42@protonmail.com>
2024-10-15 02:36:46 +0200

    Normed files;

--- minitalk.h
+++ minitalk.h
@@ -6,11 +6,11 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 01:20:11 by epinaud           #+#    #+#             */
-/*   Updated: 2024/10/15 02:03:18 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/10/15 02:36:20 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	MINITALK_H
+#ifndef MINITALK_H
 # define MINITALK_H
 # define _POSIX_SOURCE 200809L
 # define _XOPEN_SOURCE 700
@@ -20,14 +20,14 @@
 
 typedef struct s_client
 {
-	int		pid;
-	size_t		bits_count;
+	int				pid;
+	size_t			bits_count;
 	unsigned int	msglen;
-	char	*msg;
+	char			*msg;
 	unsigned char	c;
-	int		byte_mask;
+	int				byte_mask;
 	unsigned int	mxint_mask;
 }			t_client;
 
-void set_sigaction(void (sighandle)(int, siginfo_t *, void *));
+void	set_sigaction(void (sighandle)(int, siginfo_t *, void *));
 #endif
--- minitalk_utils.c
+++ minitalk_utils.c
@@ -6,22 +6,22 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 01:16:17 by epinaud           #+#    #+#             */
-/*   Updated: 2024/10/14 01:35:39 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/10/15 02:33:20 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void set_sigaction(void (sighandle)(int, siginfo_t *, void *))
+void	set_sigaction(void (sighandle)(int, siginfo_t *, void *))
 {
-	struct sigaction act;
+	struct	sigaction act;
 
 	ft_bzero(&act, sizeof(act));
 	sigemptyset(&act.sa_mask);
 	act.sa_flags = SA_SIGINFO;
 	act.sa_sigaction = sighandle;
 	if (sigaction(SIGUSR1, &act, NULL) < 0)
-		return ;	
+		return ;
 	if (sigaction(SIGUSR2, &act, NULL) < 0)
 		return ;
 }
