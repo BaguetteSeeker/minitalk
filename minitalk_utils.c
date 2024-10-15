dd304a28b7491d5f47f3b73ebf77f09f235c028e
epinaud <enzo.pinaud+42@protonmail.com>
2024-10-15 17:21:34 +0200

    Implemented check_pid() & print_err();

--- minitalk_utils.c
+++ minitalk_utils.c
@@ -6,7 +6,7 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 01:16:17 by epinaud           #+#    #+#             */
-/*   Updated: 2024/10/15 02:33:20 by epinaud          ###   ########.fr       */
+/*   Updated: 2024/10/15 17:15:58 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	set_sigaction(void (sighandle)(int, siginfo_t *, void *))
 {
-	struct	sigaction act;
+	struct sigaction	act;
 
 	ft_bzero(&act, sizeof(act));
 	sigemptyset(&act.sa_mask);
@@ -24,4 +24,34 @@ void	set_sigaction(void (sighandle)(int, siginfo_t *, void *))
 		return ;
 	if (sigaction(SIGUSR2, &act, NULL) < 0)
 		return ;
+}
+
+int	check_pid(char *pid)
+{
+	while (*pid++)
+	{
+		if (!ft_strchr("0123456789", *pid))
+			return (1);
+	}
+	return (0);
+}
+
+int	print_err(int code)
+{
+	char	*msg;
+
+	if (code == 1)
+		msg = "Error during signal transmission : transfer aborted";
+	else if (code == 2)
+		msg = "Failled to return signal (to client)";
+	else if (code == 3)
+		msg = "Server missing client's PID";
+	else if (code == 4)
+		msg = "Invalid param count sent to client";
+	else if (code == 5)
+		msg = "Invalid PID";
+	else if (code == 6)
+		msg = "Invalid param count sent to client";
+	ft_putendl_fd(msg, 1);
+	return (0);
 }
